e48fe1b0aa069a784d4c28f81656d091f58b0637
coder-mizan01 <p.mizan2441@gmail.com>
2023-11-06 02:28:52 +0600

    complete ollyo task

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,42 +1,50 @@
 import React, { useState } from "react";
-import { Link } from "react-dom";
-import { draggableImages as initialImages } from "./DraggalbeAPI";
+import { draggableImages as initialImages } from "./Images";
+
+import { BiSolidImage } from "react-icons/bi";
 //css
 import "./Gallery.css";
 
 const Gallery = () => {
   const [dragstartElm, setDragstartElm] = useState(null);
   const [dragEnterSrc, setDragEnterSrc] = useState("");
-  const [draggableImages, setDraggableImages] = useState(initialImages); // Manage images using state
+  const [dynamicClass, setDynamicClass] = useState(false);
 
+  // Function to handle the start of a drag operation
   const handleDragStart = (e) => {
     e.dataTransfer.setData("text/plain", e.target.src);
     setDragstartElm(e.target);
   };
-
+  
+// Function to handle the drag enter event
   const handleDragEnter = (e) => {
     e.preventDefault();
     setDragEnterSrc(e.target.src);
   };
 
+  // Function to handle the drag over event
   const handleDragOver = (e) => {
     e.preventDefault();
+    setDynamicClass(true);
 
     // Swap only the dragstartElm image src
     if (dragstartElm && e.target.src !== dragstartElm.src) {
-      const tempSrc = e.target.src;
       e.target.src = dragstartElm.src;
     }
   };
 
+  // Function to handle the drag Leave event
   const handleDragLeave = (e) => {
     e.preventDefault();
+    setDynamicClass(false);
+
     // Revert the src attributes
     e.target.src = dragEnterSrc;
   };
 
   const handleDrop = (e) => {
     e.preventDefault();
+    setDynamicClass(false);
 
     const tempSrc = e.target.src;
     e.target.src = tempSrc;
@@ -48,47 +56,79 @@ const Gallery = () => {
     setDragstartElm(null);
   };
 
+  // get the value of initialImages in the  draggableImages
+  const [draggableImages, setDraggableImages] = useState(initialImages);
+
+  //initialize it as an array and filled false for each element
   const [checkedImages, setCheckedImages] = useState(
     new Array(draggableImages.length).fill(false)
   );
 
+  // this function take a parameter , create copy checkedImages array and toggle the value
   const handleChecked = (index) => {
     const newCheckedImages = [...checkedImages];
     newCheckedImages[index] = !newCheckedImages[index];
     setCheckedImages(newCheckedImages);
   };
 
-  //checked how many is checked
+  //get the length how many is checked image
   const checkedCount = checkedImages.filter((isChecked) => isChecked).length;
 
-  /* const handleDeleteItems = () => {
-    const newImages = draggableImages.filter((_, index) => !checkedImages[index]);
+  //delete item function
+  const handleDeleteItems = () => {
+    const newImages = draggableImages.filter(
+      (_, index) => !checkedImages[index]
+    );
 
-    // Update the draggableImages state with the new array of images
     setDraggableImages(newImages);
-  };*/
+
+    //set check images false
+    setCheckedImages(checkedImages.fill(false));
+  };
 
   return (
     <>
       <div className="Gallery">
-        
         <div className="header">
           <p>
-            {checkedCount === 0 ? `Gallery` : `${checkedCount} image select`}
+            {checkedCount === 0 ? (
+              `Gallery`
+            ) : checkedCount === 1 ? (
+              <>
+                <input type="checkbox" defaultChecked />
+                {checkedCount} File Selected
+              </>
+            ) : (
+              <>
+                <input type="checkbox" defaultChecked />
+                {checkedCount} Files Selected
+              </>
+            )}
           </p>
-          <p>{"delete items"}</p>
+
+          <p onClick={handleDeleteItems}>
+            {checkedCount === 0
+              ? ""
+              : checkedCount === 1
+              ? "Delete File"
+              : "Delete Files"}
+          </p>
         </div>
 
         {draggableImages.map((draggableImage, index) => (
-
-          <div className="draggable" key={index}>
+          <div
+            style={dynamicClass ? { border: "1px dotted black" } : {}}
+            className={checkedImages[index] ? "draggable selected " : "draggable"}
+            key={index}
+          >
             <input
               type="checkbox"
               checked={checkedImages[index]}
               onChange={() => handleChecked(index)}
+              className="input_check"
             />
             <img
-              className={checkedImages[index] ? "checked" : ""}
+              className={checkedImages[index] ? "checked" : " "}
               src={draggableImage.props.src}
               alt="pic"
               onDrag={(e) => handleDragStart(e)}
@@ -100,6 +140,11 @@ const Gallery = () => {
             />
           </div>
         ))}
+
+        <div className="add_image">
+          <BiSolidImage className="add_img_icon" />
+          <p>Add Images</p>
+        </div>
       </div>
     </>
   );
--- src/Images.jsx
+++ src/Images.jsx
@@ -1,4 +1,5 @@
 export const draggableImages = [
+
   <img src="image-1.webp" draggable="true" alt="pic" />,
   <img src="image-2.webp" draggable="true" alt="pic" />,
   <img src="image-3.webp" draggable="true" alt="pic" />,
@@ -10,4 +11,5 @@ export const draggableImages = [
   <img src="image-9.jpg" draggable="true" alt="pic" />,
   <img src="image-10.webp" draggable="true" alt="pic" />,
   <img src="image-11.webp" draggable="true" alt="pic" />,
+
 ];
